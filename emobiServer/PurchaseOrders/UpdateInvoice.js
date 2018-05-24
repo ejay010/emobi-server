a2a1eaaf8968eddcb85ddd49f8c74d8a8b939d58
ejay010 <ejay010@gmail.com>
2018-05-24 19:31:08 -0400

    updates updates

--- emobiServer/PurchaseOrders/UpdateInvoice.js
+++ emobiServer/PurchaseOrders/UpdateInvoice.js
@@ -2,7 +2,6 @@ const Invoice = require('./PurchaseOrderModel.js');
 function UpdateInvoice(req, res, error) {
   Invoice.findById(req.params.purchaseOrderId).then((result) => {
     //will recieve guest list with updated status
-    console.log(result);
     let new_guestlist = req.body.GuestList
     let current_guestlist = result.contents
 
@@ -14,24 +13,40 @@ function UpdateInvoice(req, res, error) {
         error: "Guest list comparison does not match"
       })
     } else {
+      let changeCount = 0
       new_guestlist.forEach((spot, spot_index) => {
         if (spot.guest_spot == false) { // check to see if we are on a rsvp
-          if (spot.email == current_guestlist[spot_index].email) { // comparison of list contents and verify person is on both lists
+          if (spot.email == current_guestlist[spot_index].email) { // compare list contents and verify person is on both lists
             if (spot.outstanding != current_guestlist[spot_index].outstanding) { // check to see if this person checked in
-              result.contents[spot_index].outstanding = spot.outstanding // This line updates the document's record
+              if ((spot.outstanding == false) && (current_guestlist[spot_index].outstanding == true)) {
+                // person has arrived update, document record
+                changeCount += 1
+                result.contents[spot_index].outstanding = spot.outstanding // This line updates the document's record
+              }
             }
           }
         } else {
-          result.contents[spot_index].outstanding = spot.outstanding // this is for guest spots
+          if ((spot.outstanding == false) && (current_guestlist[spot_index].outstanding == true)) {
+            changeCount += 1
+            result.contents[spot_index].outstanding = spot.outstanding // this is for guest spots
+          }
         }
-      })
-      result.save().then((response) => {
+      }) // end of foreach scan
+      if (changeCount > 0) {
+        result.save().then((response) => {
+          res.send({
+            success: true,
+            message: "Invoice Updated",
+            invoice: response
+          })
+        })
+      } else {
         res.send({
           success: true,
-          message: "Invoice Updated",
-          invoice: response
+          message: "Nothing Changed",
+          invoice: result
         })
-      })
+      }
     }
 
   })
