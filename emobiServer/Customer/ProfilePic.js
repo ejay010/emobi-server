157c0f3166073c3d6a24912c76fbcb68af2a63b3
ejay010 <ejay010@gmail.com>
2018-06-12 23:50:22 -0400

    yooooo

--- emobiServer/Customer/ProfilePic.js
+++ emobiServer/Customer/ProfilePic.js
@@ -14,13 +14,20 @@ function ProfilePic(req, res, error) {
         Customer.findById(req.user._id).then((customer) => {
           let stringFile = JSON.stringify(req.file)
           customer.ProfilePic = stringFile
-          customer.save(function (err, updatedCustomer) {
-            console.log(updatedCustomer);
+          customer.save().then((result) => {
+            let updatedCustomer = {
+              dob: result.dob,
+              username: result.username,
+              email: result.email,
+              status: result.status,
+              ProfilePic: result.ProfilePic
+            }
+            res.send({
+              success: true,
+              messsage: "Picture Uploaded",
+              user: updatedCustomer
+            })
           })
-        })
-        res.send({
-          success: true,
-          message: "Picture Uploaded"
         })
       }
     })
