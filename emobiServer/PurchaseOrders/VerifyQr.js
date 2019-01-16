ba3eecc601727e73740b4cb27634e196734c81f2
ejay010 <ejay010@gmail.com>
2019-01-15 20:53:16 -0500

    Update verify process

--- emobiServer/PurchaseOrders/VerifyQr.js
+++ emobiServer/PurchaseOrders/VerifyQr.js
@@ -1,12 +1,26 @@
 const Invoice = require('./Invoice.js');
 
 function VerifyQr(req, res, error) {
-  res.send(req.body)
-  // Invoice.findById(req.params.invoice_id).then((results) => {
-  //   if (results._id == req.body.invoice_id) {
-  //
-  //   }
-  // })
+  // res.send(req.body)
+  Invoice.findById(req.params.invoice_id).then((results) => {
+    // scanner must be on appropriate event and the invoice must be for that event
+    if (results != null) {
+      if (results._id == req.body.scanner_event_id) {
+        res.send({invoice: results, success: true})
+      } else {
+        res.send({
+          success: false,
+          message: 'Incorrect Event'
+        })
+      }
+    } else {
+      res.send({
+        success: false,
+        message: 'Error Locating Invoice'
+      })
+    }
+
+  })
 }
 
 module.exports = VerifyQr
