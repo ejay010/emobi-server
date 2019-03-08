3fe75fa2c622f9201cd0359fc7e90f1cb09cf92c
ejay010 <ejay010@gmail.com>
2019-03-08 18:46:30 -0500

    asdf

--- emobiServer/Tickets/CodeReaderValidate.js
+++ emobiServer/Tickets/CodeReaderValidate.js
@@ -1,19 +1,17 @@
-const request = require('request');
-const xml = require('xml');
+const builder = require('xmlbuilder');
 function validateScan(req, res, error) {
 console.log('we got a hit');
 console.log(req.body);
-    let WebHookUrl = 'https://hooks.slack.com/services/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'
-    request.post(WebHookUrl, req.body)
-    // res.send(req.body)
-    let responsebody = {
-      message: [{
-        status: 1}, {        text: 'Thanks for playing'
-}]
-    }
-    let xmlstring = xml(responsebody)
-    console.log(xmlstring);
-    res.send(xmlstring)
+    let responsebody = builder.create({
+      message: {
+        status: 1,
+        text: 'Thanks for playing'
+      }
+    })
+    // let xmlstring = xml(responsebody)
+    console.log(responsebody);
+    res.type('application/xml')
+    res.send(responsebody)
 }
 
 module.exports = validateScan
