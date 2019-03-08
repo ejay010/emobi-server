6cdb8d85c9b5789f1c3509079ddcbb59283fe4ac
ejay010 <ejay010@gmail.com>
2019-03-08 18:33:23 -0500

    asdf

--- emobiServer/Tickets/CodeReaderValidate.js
+++ emobiServer/Tickets/CodeReaderValidate.js
@@ -6,12 +6,12 @@ console.log(req.body);
     let WebHookUrl = 'https://hooks.slack.com/services/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'
     request.post(WebHookUrl, req.body)
     // res.send(req.body)
-    let responsebody = [{
-      message: {
+    let responsebody = {
+      message: [{
         status: 1,
         text: 'Thanks for playing'
-      }
-    }]
+      }]
+    }
     let xmlstring = xml(responsebody)
     console.log(xmlstring);
     res.send(xmlstring)
