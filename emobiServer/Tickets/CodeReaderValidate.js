08f69c86bfb89dfd1005635ff18caf40296ae5f9
ejay010 <ejay010@gmail.com>
2019-03-08 19:16:47 -0500

    asdfasd

--- emobiServer/Tickets/CodeReaderValidate.js
+++ emobiServer/Tickets/CodeReaderValidate.js
@@ -1,12 +1,16 @@
 const builder = require('xmlbuilder');
 function validateScan(req, res, error) {
 console.log('we got a hit2');
-    let responsebody = builder.create({
-      message: {
-        status: 1,
-        text: 'Thanks for playing'
-      }
-    })
+    // let responsebody = builder.create({
+    //   message: {
+    //     status: 1,
+    //     text: 'Thanks for playing'
+    //   }
+    // })
+    let responsebody = builder.create('message')
+    responsebody.ele('status', 1)
+    responsebody.ele('text', 'Thanks for playing')
+    console.log(responsebody);
     // console.log(res);
     // let responsebody = 'hello'
     // let xmlstring = xml(responsebody)
