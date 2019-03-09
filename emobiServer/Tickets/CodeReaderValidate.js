3a82dca0a016e929faa0b62e5d84e02f1751f182
ejay010 <ejay010@gmail.com>
2019-03-09 16:54:16 -0500

    asdfasdfas

--- emobiServer/Tickets/CodeReaderValidate.js
+++ emobiServer/Tickets/CodeReaderValidate.js
@@ -1,7 +1,12 @@
 const builder = require('xml-js');
 function validateScan(req, res, error) {
 console.log('we got a hit2');
-console.log(req);
+console.log('params');
+console.log(req.params);
+console.log('query');
+console.log(req.query);
+console.log('body');
+console.log(req.body);
 let lead = '<?xml version="1.0" encoding="UTF-8"?>'
 
     let resj = builder.js2xml({
