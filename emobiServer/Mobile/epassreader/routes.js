ca60b06fcf44d2818e706bcc3f3db16ff47c6295
ejay010 <ejay010@gmail.com>
2019-03-22 21:24:53 -0400

    jlkkjlk

--- emobiServer/Mobile/epassreader/routes.js
+++ emobiServer/Mobile/epassreader/routes.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
+const Events = require('../../Events').Class;
 function routes(app, passport) {
   app.post('/mobile/epassreader/login', passport.authenticate('administrators', {session: false}),
     function (req, res, error) {
       let user = req.user
       const token = jwt.sign({user: user}, 'batman')
     res.json({user, token})
+    })
+    app.get('/mobile/epassreader/events', passport.authenticate('jwt', {session: false}), function (req, res, error) {
+      let events = Events.find({status: "published"}).exec(function (err, results) {
+        res.json({events: results})
+      })
     })
 }
 
