011ab43ad4c811bbdcf6173f9d10c0f060101bf5
ejay010 <ejay010@gmail.com>
2019-05-15 18:20:53 -0400

    asdfasd

--- emobiServer/Mobile/epassreader/routes.js
+++ emobiServer/Mobile/epassreader/routes.js
@@ -8,8 +8,8 @@ function routes(app, passport) {
     // function (req, res, error) {
     //   let user = req.user
     //   const token = jwt.sign({user: user}, 'batman')
-    // res.json({user, token})
-    })
+    // res.json({user, token})}
+    )
     app.get('/mobile/epassreader/events', passport.authenticate('jwt', {session: false}), function (req, res, error) {
       let events = Events.find({status: "published"}).exec(function (err, results) {
         res.json({events: results})
